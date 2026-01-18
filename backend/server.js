ef0fd8ddecc0ecfcb16dcc1fbae2b67e50ec1ec3
Araba <grantaidooaraba@gmail.com>
2026-01-18 21:59:19 +0000

    changed the port to 5001

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 const listings = require("./data/listings.json");
 
